9b495014aa13691211f9f91a803ec9d074444ff6
Angelo Pirola <angelo9871@gmail.com>
2026-07-21 10:15:37 +0200

    Add generic IEntity<TKey> interface with Id property
    A new IEntity<TKey> interface was introduced in the Repository.Domain.Entities.Interfaces namespace. It defines a generic Id property where TKey implements IEquatable<TKey>.

---
+++ src/Repository.Domain/Entities/Interfaces/IEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace Repository.Domain.Entities.Interfaces;
+
+// Interfaccia che espone la chiave Id in forma generica
+public interface IEntity<TKey> where TKey : IEquatable<TKey>
+{
+    TKey Id { get; }
+}
